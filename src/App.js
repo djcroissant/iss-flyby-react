79f476f870e240596034a1b5e60f00b320feaab8
Derek Covey <derek.covey@gmail.com>
2018-06-19 17:18:28 -0700

    Create form for inputs to iss api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function InputElement(props) {
+//   return (
+//     <label for={props.id}>{props.label}</label>
+//     <input id={props.id} type="number" name={props.name} />
+//   );
+// }
+//
+// function InputSet(props) {
+//   return (
+//     <div className="UserInput">
+//       <InputElement id="latitude" label="Latitude:" name="latitude" />
+//
+//     </div>
+//   );
+// }
+
 class Header extends Component {
   render() {
     return (
@@ -13,22 +29,103 @@ class Header extends Component {
   }
 }
 
-class Content extends Component {
+class IssApiForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: '',
+      longitude: '',
+      altitude: '',
+      number: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log('The form was submitted');
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Latitude:
+          <input
+            name="latitude"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="0.00001"
+            min="0"
+            max="90"
+          />
+        </label>
+        <br />
+        <label>
+          Longitude:
+          <input
+            name="longitude"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="0.00001"
+            min="-180"
+            max="180"
+          />
+        </label>
+        <br />
+        <label>
+          Altitude:
+          <input
+            name="altitude"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="1"
+            min="0"
+            max="45000"
+          />
+        </label>
+        <br />
+        <label>
+          Number of passovers:
+          <input
+            name="number"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+            step="1"
+            min="0"
+            max="100"
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Content />
+        <IssApiForm />
       </div>
     );
   }

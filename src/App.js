6a022840e8980498420c51033e6d728055304bb8
Derek Covey <derek.covey@gmail.com>
2018-06-20 12:15:27 -0700

    Render API response in table

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ class IssApiForm extends Component {
       latitude: '',
       longitude: '',
       altitude: '',
-      number: ''
+      number: '',
+      flybys: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -50,102 +51,119 @@ class IssApiForm extends Component {
       "&n=" +
       this.state.number.toString()
       )
-      .then(response => console.log(response))
+      .then(response => {
+        // console.log(response.data);
+        this.setState({
+          'flybys': response.data.response
+        });
+      })
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Latitude:
-          <input
-            name="latitude"
-            type="number"
-            value={this.state.latitude}
-            onChange={this.handleChange}
-            step="0.00001"
-            min="0"
-            max="90"
-          />
-        </label>
-        <br />
-        <label>
-          Longitude:
-          <input
-            name="longitude"
-            type="number"
-            value={this.state.longitude}
-            onChange={this.handleChange}
-            step="0.00001"
-            min="-180"
-            max="180"
-          />
-        </label>
-        <br />
-        <label>
-          Altitude:
-          <input
-            name="altitude"
-            type="number"
-            value={this.state.altitude}
-            onChange={this.handleChange}
-            step="1"
-            min="0"
-            max="45000"
-          />
-        </label>
-        <br />
-        <label>
-          Number of passovers:
-          <input
-            name="number"
-            type="number"
-            value={this.state.number}
-            onChange={this.handleChange}
-            step="1"
-            min="0"
-            max="100"
-          />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="content">
+        <div className="iss-form">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Latitude:
+              <input
+                name="latitude"
+                type="number"
+                value={this.state.latitude}
+                onChange={this.handleChange}
+                step="0.00001"
+                min="0"
+                max="90"
+              />
+            </label>
+            <br />
+            <label>
+              Longitude:
+              <input
+                name="longitude"
+                type="number"
+                value={this.state.longitude}
+                onChange={this.handleChange}
+                step="0.00001"
+                min="-180"
+                max="180"
+              />
+            </label>
+            <br />
+            <label>
+              Altitude:
+              <input
+                name="altitude"
+                type="number"
+                value={this.state.altitude}
+                onChange={this.handleChange}
+                step="1"
+                min="0"
+                max="45000"
+              />
+            </label>
+            <br />
+            <label>
+              Number of passovers:
+              <input
+                name="number"
+                type="number"
+                value={this.state.number}
+                onChange={this.handleChange}
+                step="1"
+                min="0"
+                max="100"
+              />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="flyby-table">
+          {this.state.flybys !== '' ?
+          <FlybyTable response={this.state.flybys} /> : null}
+        </div>
+      </div>
     );
   }
 }
 
-function FlybyRows(props) {
-  const flybys = props.response;
-  const flybyRows = flybys.map((flyby) =>
-    <tr key={flyby.risetime.toString()}>
-      <td>{flyby.risetime}</td>
-      <td>{flyby.duration}</td>
-    </tr>
-  );
-  return (
-    <tbody>
-      {flybyRows}
-    </tbody>
-  );
+class FlybyRows extends Component {
+  render() {
+    const flybys = this.props.response;
+    const flybyRows = flybys.map((flyby) =>
+      <tr key={flyby.risetime.toString()}>
+        <td>{flyby.risetime}</td>
+        <td>{flyby.duration}</td>
+      </tr>
+    );
+    return (
+      <tbody>
+        {flybyRows}
+      </tbody>
+    );
+  }
 }
 
-function FlybyTable(props) {
-  const response = [
-    {"risetime": "time1", "duration": "duration1"},
-    {"risetime": "time2", "duration": "duration2"},
-  ];
-  return (
-    <table>
-      <thead>
-      <tr>
-        <th>Rise Time</th>
-        <th>Duration</th>
-      </tr>
-      </thead>
-      <FlybyRows response={response}/>
-    </table>
-  );
+class FlybyTable extends Component {
+  render() {
+    if (this.props.flybys !== '') {
+      return (
+        <table>
+          <thead>
+          <tr>
+            <th>Rise Time</th>
+            <th>Duration</th>
+          </tr>
+          </thead>
+          <FlybyRows response={this.props.response}/>
+        </table>
+      );
+    } else {
+      return null
+    }
+  }
 }
 
 class App extends Component {
@@ -154,7 +172,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <IssApiForm />
-        <FlybyTable />
       </div>
     );
   }
